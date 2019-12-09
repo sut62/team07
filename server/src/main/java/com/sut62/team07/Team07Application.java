3b204d4d5af0608fc1de09ba027c5f2f03bd101e
infinityc2 <super_star@hotmail.co.th>
2019-12-09 11:39:45 +0700

    close - #43 ทำการนำ project backend ขึ้น git

---
+++ server/src/main/java/com/sut62/team07/Team07Application.java
@@ -0,0 +1,13 @@
+package com.sut62.team07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team07Application.class, args);
+	}
+
+}
