4749cb7a8a9b53b128615d20780be3ed0d67d1a0
infinityc2 <super_star@hotmail.co.th>
2019-12-11 02:35:58 +0700

    close #46 - ทำ cors ให้กับ controller ระดับ global

---
+++ server/src/main/java/com/sut62/team07/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.sut62.team07.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type", "Accept")
+                .allowedOrigins("http://localhost:8081");
+    }
+}
