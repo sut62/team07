ea71989296e1428d18245b50afac5ea545457a3e
nuengruethai223 <nuengruethai.kw3456@gmail.com>
2019-12-29 20:08:03 +0700

    close #36 - ทำ Entity สำหรับลงทะเบียนเรียน

---
+++ server/src/main/java/com/sut62/team07/entity/Section.java
@@ -0,0 +1,23 @@
+package com.sut62.team07.entity;
+import lombok.*;
+import javax.persistence.*;
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="SECTION") 
+public class Section {
+
+    @Id
+    @SequenceGenerator(name="section_seq",sequenceName="section_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="section_seq")
+    @Column(name = "SECTION_ID", unique = true, nullable = true)
+    private @NonNull Long id;
+
+    private @NonNull String sec;
+
+    // *-1 with subject
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Course.class)
+    private Course subInSec;
+	
+
+}
