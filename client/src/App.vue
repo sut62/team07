564c4d3df95be53c7c1ff14b924fdd280cc23c24
NattapongB601 <fanta041241@gmail.com>
2020-01-06 21:36:27 +0700

    ทำ entity ระบบสมัครบัญชีผู้class student -close #31

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,49 @@
-<template>
-  <v-app>
-    <router-view/>
+<template >
+
+  <v-app >
+    <div>
+<v-app-bar app class ="deep-orange lighten-1" >
+    
+      <v-toolbar-title v-if="this.ids != null" class="headline text-uppercase">
+        
+        <span class="font-weight-light"> ระบบลงทะเบียนเรียน</span>
+        &nbsp;
+        <span>{{this.ids}}</span>
+          
+         <v-btn text href="/ " target="_self"></v-btn>
+          <v-btn text href="/Register" target="_self">Register</v-btn>
+           <v-btn text href="/ " target="_self"></v-btn>
+            <v-btn text href="/ " target="_self"></v-btn>
+            <v-btn @click="logOut" text target="_self">LOGOUT</v-btn>
+        </v-toolbar-title>
+        
+   </v-app-bar>
+    </div>
+<v-content class="orange lighten-4">
+     
+      <router-view />
+    </v-content>
   </v-app>
+  
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      valid:false,
+    check: false,
+    ids: localStorage.getItem("siteUser")
+    };
+  },
+  methods:{
+    logOut(){
+      localStorage.removeItem("siteId");
+      localStorage.removeItem("siteUser");
+      this.ids=null;
+      this.$router.push("/");
+},
+  }
+   };
+</script>
--- server/src/main/java/com/sut62/team07/entity/Student.java
+++ server/src/main/java/com/sut62/team07/entity/Student.java
@@ -1,7 +1,18 @@
-package com.sut62.team07.entity;
+package com.cpe.backend.entity;
 import lombok.*;
-import javax.persistence.*;
-
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import  com.cpe.backend.entity.Major;
+import  com.cpe.backend.entity.TypeName;
+import  com.cpe.backend.entity.Year;
 @Data
 @Entity
 @NoArgsConstructor
@@ -9,16 +20,69 @@ import javax.persistence.*;
 public class Student {
 
     @Id
-    @SequenceGenerator(name="student_seq",sequenceName="student_seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="student_seq")
-    @Column(name = "STUDENT_ID", unique = true, nullable = true)
+    @SequenceGenerator(name="id_student_seq",sequenceName="id_student_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="id_student_seq")
+    @Column(name = "ID_STUDENT_ID", unique = true, nullable = true)
     private @NonNull Long id;
-
-    private @NonNull String num;
-
-  
     
 
+    @Column(name="STUDENT_ID")
+    private @NonNull String student_id;
 
+    @Column(name="STUDENT_PHONE")
+    private @NonNull String student_phone;
 
+    @Column(name="STUDENT_NAME")
+    private @NonNull String student_name;
+    
+    @Column(name="STUDENT_EMAIL")
+    private @NonNull String student_email;
+    
+    @Column(name="Password")
+    private @NonNull String password;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Major.class)
+    @JoinColumn(name = "MAJOR_ID", insertable = true)
+    private Major major;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Year.class)
+    @JoinColumn(name = "YEAR_ID", insertable = true)
+    private Year year;
+    
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = TypeName.class)
+    @JoinColumn(name = "TYPENAME_ID", insertable = true)
+    private TypeName typeName;
+
+        public void setMajor(Major major) {
+        this.major = major;
+	}
+
+	public void setStudent_id(String student_id) {
+        this.student_id=student_id;
+	}
+
+	public void setName(String student_name) {
+        this.student_name=student_name;
+	}
+
+	public void setEmail(String student_email) {
+        this.student_email=student_email;
+	}
+
+        public void setPhone(String student_phone) {
+                this.student_phone=student_phone;
+                }
+	public void setPassword(String password2) {
+        this.password=password2;
+	}
+
+        public void setTypeName(TypeName typename) {
+        this.typeName=typename;
+	}
+
+        public void setYear(Year year) {
+        this.year=year;
+	}
+
+    
 }
