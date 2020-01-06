ce755d295568be703797683d2b452430aaac9448
infinityc2 <super_star@hotmail.co.th>
2020-01-06 23:24:02 +0700

    close #57 - ทำ login ของ user

--- server/src/main/java/com/sut62/team07/controller/AuthenticationController.java
+++ server/src/main/java/com/sut62/team07/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import com.sut62.team07.request.AuthRequest;
 import com.sut62.team07.response.AuthResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,7 @@ public class AuthenticationController {
     @Autowired
     private RegistrationOfficerRepository registrationOfficerRepository;
 
+    @PostMapping("/auth")
     public AuthResponse login(@RequestBody AuthRequest auth) throws Exception {
         AuthResponse response = new AuthResponse();
         lecturerRepository.findByLecturerCode(auth.getUsername()).ifPresent(l -> {
@@ -38,7 +40,12 @@ public class AuthenticationController {
             }
         });
 
-        
+        studentRepository.findByStudentId(auth.getUsername()).ifPresent(s -> {
+            if (s.getPassword().equals(auth.getPassword())) {
+                response.setUsername(auth.getUsername());
+                response.setRole("Student");
+            }
+        });
 
         return response;
 
